f323004b02e344515976d0905740c7bc177819a5
Guilherme Araujo <guiluis.aa@gmail.com>
2020-12-16 18:43:58 -0300

    feat: input

--- src/views/index/Index.view.tsx
+++ src/views/index/Index.view.tsx
@@ -1,36 +1,10 @@
 import React, { FC } from 'react';
 
 import { Title } from '@/components/typograph/Typograph.component';
-import Button from '@/components/button/Button.component';
 
 const IndexView: FC = () => (
   <>
     <Title>Index</Title>
-    <br />
-    <br />
-    <br />
-
-    <Button>Search</Button>
-    <br />
-    <br />
-    <br />
-    <Button disabled>Search</Button>
-    <br />
-    <br />
-    <br />
-    <Button block>Search</Button>
-    <br />
-    <br />
-    <br />
-    <Button block isLoading>
-      Search
-    </Button>
-    <br />
-    <br />
-    <br />
-    <Button block isLoading disabled>
-      Search
-    </Button>
   </>
 );
 
