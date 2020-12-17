bcfb7eaf39c6b867b15343d73be8833a6b3313d0
Guilherme Araujo <guiluis.aa@gmail.com>
2020-12-17 20:23:54 -0300

    feat: history page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,11 +6,15 @@ import Layout from './components/layout/Layout.component';
 import GlobalStyle from './shared-styles/GlobalStyle';
 import { darkTheme, lightTheme } from './themes';
 import useTheme from '@/io/redux/theme/useTheme.hook';
+import { toLoadable } from './utils';
 
 // Views
-import Redirect404View from './views/404/Redirect404.view';
-import IndexView from './views/index/Index.view';
-import SearchView from './views/search/Search.view';
+import Redirect404View from '@/views/404/Redirect404.view';
+import IndexView from '@/views/index/Index.view';
+import SearchView from '@/views/search/Search.view';
+
+// Lazy loading views
+const HistoryView = toLoadable(() => import('@/views/history/History.view'));
 
 const Routes: FC = () => {
   const { theme } = useTheme();
@@ -23,6 +27,7 @@ const Routes: FC = () => {
           <Switch>
             <Route exact path="/" component={IndexView} />
             <Route exact path="/search" component={SearchView} />
+            <Route exact path="/history" component={HistoryView} />
 
             <Route component={Redirect404View} />
           </Switch>
