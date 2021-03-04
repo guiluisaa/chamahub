99333c31cd78fe739d95076df648cd6106638afd
Guilherme Araujo <guiluis.aa@gmail.com>
2021-03-04 16:32:22 -0300

    refactor: test folder

--- jest.config.js
+++ jest.config.js
@@ -21,14 +21,14 @@ module.exports = {
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/tests/__mocks__/fileMock.ts',
+      '<rootDir>/test/__mocks__/fileMock.ts',
     '@/(.*)$': '<rootDir>/src/$1',
 
     '^styled-components': '<rootDir>/node_modules/styled-components',
   },
-  testMatch: ['<rootDir>/tests/**/*.test.+(ts|tsx|js|jsx)'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/src/'],
-  setupFilesAfterEnv: ['jest-canvas-mock', '<rootDir>/tests/setup.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.+(ts|tsx|js|jsx)'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: ['jest-canvas-mock', '<rootDir>/test/setup.ts'],
   verbose: true,
   testURL: 'http://localhost/',
   snapshotSerializers: ['enzyme-to-json/serializer'],
