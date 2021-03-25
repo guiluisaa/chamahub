3a129cfbe51a817cde6406b1d22053953d416000
Guilherme Araujo <guiluis.aa@gmail.com>
2021-03-24 21:48:34 -0300

    style: remove line breaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react';
 import { Provider } from 'react-redux';
 import { hot } from 'react-hot-loader';
-
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { persistor, store } from '@/io/redux/store';
-
 import Routes from './Routes';
 
 const App: FC = () => (
