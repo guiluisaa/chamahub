9a7c746411d2c212ab4b8586e8e9e29bdf714292
Guilherme Araujo <guiluis.aa@gmail.com>
2022-05-04 18:13:08 -0300

    Revert "refactor: repo card"
    This reverts commit b6cacb5c901259a2f8c6aadc5e11c2549054a284.

--- src/components/repo-card/RepoCard.component.tsx
+++ src/components/repo-card/RepoCard.component.tsx
@@ -1,8 +1,10 @@
+import React, { FC } from 'react';
 import styled from 'styled-components';
 
+import { RepoModel } from '@/models/Repo.model';
 import { Paragraph } from '../typograph/Typograph.component';
 
-export const Wrapper = styled.div`
+const Wrapper = styled.div`
   width: 100%;
   background-color: ${({ theme }) =>
     theme.components.card.backgroundColorSecondary};
@@ -12,22 +14,22 @@ export const Wrapper = styled.div`
   border-radius: ${({ theme }) => theme.border.radius};
 `;
 
-export const Name = styled(Paragraph)`
+const Name = styled(Paragraph)`
   font-weight: ${({ theme }) => theme.font.weight.bold};
 `;
 
-export const Description = styled(Paragraph)`
+const Description = styled(Paragraph)`
   font-size: 12px;
   line-height: 16px;
   margin-top: 5px;
 `;
 
-export const Url = styled(Description)`
+const Url = styled(Description)`
   margin-top: 20px;
   word-wrap: break-word;
 `;
 
-export const Link = styled.a`
+const Link = styled.a`
   text-decoration: none;
   color: ${({ theme }) => theme.color.blue};
 
@@ -39,3 +41,23 @@ export const Link = styled.a`
     color: ${({ theme }) => theme.color.blue};
   }
 `;
+
+type RepoCardProps = {
+  repo: RepoModel;
+};
+
+const RepoCard: FC<RepoCardProps> = ({ repo }) => (
+  <Wrapper>
+    <Link href={repo.html_url} target="_blank">
+      <Name>{repo.name}</Name>
+    </Link>
+
+    <Description color="secondary">{repo.description}</Description>
+
+    <Link href={repo.html_url} target="_blank">
+      <Url>{repo.html_url}</Url>
+    </Link>
+  </Wrapper>
+);
+
+export default RepoCard;
