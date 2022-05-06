a48dcd1a395765f989441674f202b002e0e94dfe
Guilherme Araujo <guiluis.aa@gmail.com>
2022-05-06 00:04:44 -0300

    reafctor: 404 view

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -9,7 +9,7 @@ import useTheme from '@/io/redux/theme/useTheme.hook';
 import { toLoadable } from './utils';
 
 // Views
-import Redirect404View from '@/views/404/Redirect404.view';
+import Redirect404View from '@/views/404';
 import IndexView from '@/views/index/Index.view';
 import SearchView from '@/views/search/Search.view';
 
