94266b9c6ce9b873496c0ee0c012dda02dcf83e3
Guilherme Araujo <guiluis.aa@gmail.com>
2022-05-10 21:17:46 -0300

    refactor: flexbox

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import Flexbox from '@/shared-styles/Flexbox.css';
-
 export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.background.color.primary};
 `;
@@ -14,9 +12,10 @@ export const HeaderWrapper = styled.div`
 `;
 
 export const Content = styled.div`
+  display: flex;
+
   padding-top: 70px;
   padding-bottom: 30px;
 
   min-height: 100vh;
-  ${Flexbox}
 `;
--- src/views/Search/styles.ts
+++ src/views/Search/styles.ts
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 import SharedContainer from '@/components/Container';
-import Flexbox from '@/shared-styles/Flexbox.css';
+
 import SharedAlert from '@/components/Alert';
 
 export const Wrapper = styled(SharedContainer)`
   width: 100%;
-  ${Flexbox}
+  display: flex;
   flex-direction: column;
   justify-content: center;
 `;
